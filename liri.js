35c593cfa9fede18277391f407130d4a85288662
Bijan <bijansemnani@Bijans-MacBook-Pro.local>
2018-03-11 10:23:27 -0700

    added my-tweets secion to the code

--- liri.js
+++ liri.js
@@ -7,5 +7,14 @@ var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-console.log(JSON.stringify(spotify,null,2));
-console.log(JSON.stringify(client,null,2));
+var option = process.argv[2];
+
+if(option === "my-tweets"){
+  client.get('statuses/user_timeline', {screen_name: 'beeej236',count:'20'},
+  function(error, tweets, response) {
+    for (var i = 0; i < tweets.length; i++) {
+      console.log("-------------------------");
+      console.log(tweets[i].text);
+    }
+  });
+}
